94f10d2fac3af2e937eb9d91464f6320abc1456e
jialer <20021576@myrp.edu.sg>
2021-07-27 12:03:43 +0800

    did custom listview

---
+++ app/src/main/java/com/myapplicationdev/android/p10_ndpsongs_clv/CustomAdapter.java
@@ -0,0 +1,69 @@
+package com.myapplicationdev.android.p10_ndpsongs_clv;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<Song> songList;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Song> objects){
+        super(context, resource, objects);
+
+        parent_context = context;
+        layout_id = resource;
+        songList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        TextView tvTitle = rowView.findViewById(R.id.textViewTitle);
+        TextView tvDate = rowView.findViewById(R.id.textViewDate);
+        TextView tvStars = rowView.findViewById(R.id.textViewStars);
+        TextView tvSingers = rowView.findViewById(R.id.textViewSinger);
+
+        Song currentSong = songList.get(position);
+
+        tvTitle.setText(currentSong.getTitle());
+        tvDate.setText(String.valueOf(currentSong.getYearReleased()));
+        if (currentSong.getStars() == 1){
+            String stars = "*";
+            tvStars.setText(stars);
+        } else if (currentSong.getStars() == 2){
+            String stars = "* *";
+            tvStars.setText(stars);
+        } else if (currentSong.getStars() == 3){
+            String stars = "* * *";
+            tvStars.setText(stars);
+        } else if (currentSong.getStars() == 4){
+            String stars = "* * * *";
+            tvStars.setText(stars);
+        } else if (currentSong.getStars() == 5){
+            String stars = "* * * * *";
+            tvStars.setText(stars);
+        }
+        tvSingers.setText(currentSong.getSingers());
+
+        return rowView;
+    }
+}
